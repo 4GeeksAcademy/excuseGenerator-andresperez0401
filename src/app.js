2457b67a1fc5bba4d3613247484dae766de68aab
Andres Perez <andresperez0401@gmail.com>
2025-01-15 10:49:37 -0400

    Excuse Generator Completed

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// /* eslint-disable */
+// import "bootstrap";
+// import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
+// Excusas
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+
+//Funcion que se ejecuta al cargar la pagina	
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let excuse = getExcuse();
+  document.getElementById("excuse").innerHTML = excuse;
 };
+
+
+//Funcion que en base a los arregloa de datos para crear la excusa, devuelve una excusa random
+function getExcuse(){
+  //numero random entre 1 y la longitud del array; Quien fue
+  let quien = who[Math.floor(Math.random() * who.length)];
+
+  //Quien hizo la accion
+  let accion = action[Math.floor(Math.random() * action.length)];
+
+  //Que hizo
+  let que = what[Math.floor(Math.random() * what.length)];
+
+  //Cuando
+  let cuando = when[Math.floor(Math.random() * when.length)];
+
+  let finalExcuse = quien + " " + accion + " " + que + " " + cuando;
+
+  return finalExcuse
+}
